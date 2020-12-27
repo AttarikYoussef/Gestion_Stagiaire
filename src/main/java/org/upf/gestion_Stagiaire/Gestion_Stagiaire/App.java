3a294c647a7924d24a79dfe45c80d2617f22f631
USSEF98 <attarikyoussef8@gmail.com>
2020-12-27 14:51:43 +0000

    Creation de Projet -Gestion de Stagiaire-

---
+++ src/main/java/org/upf/gestion_Stagiaire/Gestion_Stagiaire/App.java
@@ -0,0 +1,13 @@
+package org.upf.gestion_Stagiaire.Gestion_Stagiaire;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
