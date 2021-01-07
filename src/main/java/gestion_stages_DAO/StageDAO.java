23e1a62207c7773207c286141215f3c4868c0564
USSEF98 <attarikyoussef8@gmail.com>
2021-01-07 00:05:47 +0000

    Ajout de Controller et class DAO

---
+++ src/main/java/gestion_stages_DAO/StageDAO.java
@@ -0,0 +1,41 @@
+package gestion_stages_DAO;
+
+import java.util.List;
+
+import org.upf.gestion_Stagiaire.Entity.Stage;
+
+import gestion_stagiaire_interface.IntGlobale;
+
+public class StageDAO implements IntGlobale<Stage>{
+
+	@Override
+	public boolean ajouter(Stage t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Stage findByID(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Stage> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Stage obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
